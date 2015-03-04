a091743141bf7c026561aaaf546521b07b95f5a8
Rex McKinnon <rex.mckinnon1@gmail.com>
2015-03-04 02:46:41 -0600

    Updated graph looks, added favicon and title

--- web-assets/stracchart.js
+++ web-assets/stracchart.js
@@ -1,10 +1,6 @@
-
-
 require(["web-assets/Chartjs/Chart.min.js", "requirejs/text!bloodsugars.json"], function (Chart, bloodsugars) {
-
   var sugarDB = JSON.parse(bloodsugars);
   var sugarTable = sugarDB.results;
-
   var data = {
     labels: [],
     datasets: [
@@ -25,13 +21,14 @@ require(["web-assets/Chartjs/Chart.min.js", "requirejs/text!bloodsugars.json"], 
           datasetStrokeWidth: 2,
   };
   Chart.defaults.global.showTooltips = false;
-
   for(var i = 0; i < sugarTable.length; i++) {
       var bloodsugar = parseInt(sugarTable[i]['bloodsugar']);
       var time = sugarTable[i]['time'];
       var med = sugarTable[i]['med'];
       if(med == 'none') {
         med = 0;
+      } else {
+        med = parseInt(med) * 2
       }
       // add time datapoint to the chart
       data.labels.push(time);
@@ -40,10 +37,8 @@ require(["web-assets/Chartjs/Chart.min.js", "requirejs/text!bloodsugars.json"], 
       // add medication error bar to the chart
       data.datasets[0].error.push(parseInt(med));
   }
-
   var ctx = document.getElementById("DiabetesChart").getContext("2d");
-  ctx.canvas.height = window.innerHeight / 2;
-  ctx.canvas.width = window.innerWidth / 2;
+  ctx.canvas.height = window.innerHeight * 0.7;
+  ctx.canvas.width = window.innerWidth * 0.7;
   var chart = new Chart(ctx).Line(data, options);
-
 });
