c4ffb03ed2e94d7760afda2fa6e1d4825cb01e0e
Rex McKinnon <rex.mckinnon1@gmail.com>
2015-03-04 11:20:03 -0600

    Added several api endpoints

--- stracc.py
+++ stracc.py
@@ -12,11 +12,19 @@ def recordBStoDB(bs, t, m="none"):
     db = dataset.connect("sqlite:///db/bloodsugars.db")
     table = db['bloodsugars']
     table.insert(dict(bloodsugar=bs, time=t, med=m))
-    saveDbTabletoJson(db, 'bloodsugars')
 
-def saveDbTabletoJson(db, table):
-    dbtable = db[table]
+def saveDbTabletoJson():
+    db = dataset.connect('sqlite:///db/bloodsugars.db')
+    dbtable = db['bloodsugars']
     dataset.freeze(dbtable, format="json", filename="web-assets/bloodsugars.json")
+
+def deleteBSfromDB(id):
+    db = dataset.connect('sqlite:///db/bloodsugars.db')
+    dbtable = db['bloodsugars']
+    if id != 'all':
+        dbtable.delete(id=id)
+    else:
+        dbtable.delete()
 
 # Main bloodsugar graph
 #   Serves chart.js chart.
@@ -42,11 +50,54 @@ def postBloodSugar():
         med = req['med']
     except KeyError as err:
         return flask.json.jsonify(error = "Bad request"), 400
-    print "Bloodsugar: ", bS
-    print "Time: ", time
-    print "Meds: ", med
     recordBStoDB(bS, time, med)
+    saveDbTabletoJson()
     return flask.json.jsonify(success="true"), 200
+
+# Gets multiple blood sugar test and adds each one to the db.
+@app.route('/test/multiple', methods=['POST'])
+def postMultipleBloodSugar():
+    try:
+        req = flask.request.get_json()
+        sugars = req['bloodsugars']
+        times = req['times']
+        meds = req['meds']
+    except KeyError:
+        return flask.json.jsonify(error = 'Bad request'), 400
+
+    for i in range(len(sugars)):
+        recordBStoDB(sugars[i], times[i], meds[i])
+    saveDbTabletoJson()
+    return flask.json.jsonify(success = 'true'), 200
+
+# Accepts a json db and replaces it with the current one.
+@app.route('/test/updatedb', methods=['POST'])
+def updateDB():
+    try:
+        req = flask.request.get_json()
+        results = req['results']
+    except KeyError:
+        return flask.json.jsonify(error = 'Bad request'), 400
+
+    deleteBSfromDB('all')
+
+    for bs in results:
+        recordBStoDB(bs['bloodsugar'], bs['time'], bs['med'])
+
+    saveDbTabletoJson()
+    return flask.json.jsonify(success = 'true'), 200
+
+# Deletes the data entry with 'id'
+@app.route('/test/delete', methods=['POST'])
+def postTestDelete():
+    try:
+        req = flask.request.get_json()
+        bsid = int(req['id'])
+    except KeyError:
+        return flask.json.jsonify(error = 'Bad request'), 400
+    deleteBSfromDB(bsid)
+    saveDbTabletoJson()
+    return flask.json.jsonify(success='true'), 200
 
 if not os.path.exists('db'):
     os.mkdir('db')
